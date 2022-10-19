ab9bf98992e8c0f5b7f8190e071688826865534c
Marcio André <andremarcio1988@gmail.com>
2022-10-19 10:42:41 -0300

    funções parte 1

--- 14-ARRAY/ARRAY.js
+++ 14-ARRAY/ARRAY.js
@@ -1,12 +1,12 @@
-// let list = new Array("arroz", "feijão", "açucar", "fubá");
-// let listValor = new Array("R$ 10","R$ 8","R$ 9","R$ 10");
+let list = new Array("arroz", "feijão", "açucar", "fubá");
+let listValor = new Array("R$ 10","R$ 8","R$ 9","R$ 10");
 
-// for (let index in list) {
-//   for (let index2 in listValor)
-//     console.log(list[index], listValor[index2]);
+for (let index in list) {
+  for (let index2 in listValor)
+    console.log(list[index], listValor[index2]);
 
-// }
-// console.log(list[index])
+}
+console.log(list[index])
 
 
 let nome = ["marcio", "juliana", "pedro", "paulo",];
---
+++ 15-FUNCOES/funcoes.js
@@ -0,0 +1,11 @@
+function media(n1, n2) {
+  let nota1 = n1;
+  let nota2 = n2;
+  let media = (nota1 + nota2) / 2;
+  return media;
+}
+
+
+let resultado = media(10, 10);
+
+console.log(resultado);
