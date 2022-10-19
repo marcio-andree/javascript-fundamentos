902090e4e34e415f5ec68629d0802a5f57f075f4
Marcio André <andremarcio1988@gmail.com>
2022-10-19 10:21:53 -0300

    array e tupla

---
+++ 14-ARRAY/ARRAY.js
@@ -0,0 +1,17 @@
+// let list = new Array("arroz", "feijão", "açucar", "fubá");
+// let listValor = new Array("R$ 10","R$ 8","R$ 9","R$ 10");
+
+// for (let index in list) {
+//   for (let index2 in listValor)
+//     console.log(list[index], listValor[index2]);
+
+// }
+// console.log(list[index])
+
+
+let nome = ["marcio", "juliana", "pedro", "paulo",];
+
+for (const nomes of nome) {
+
+  console.log(nomes);
+}
