ee8c081bf59a599152b25ef2390fe661de6f293d
Marcio André <andremarcio1988@gmail.com>
2022-10-19 10:59:57 -0300

    arrow function and anonymous function

---
+++ 15-FUNCOES/arrow.js
@@ -0,0 +1,5 @@
+// função arrow
+let media = (n1, n2) => {
+  return (n1 + n2) / 2;
+}
+console.log(media(10, 8))
---
+++ 15-FUNCOES/func-anon.js
@@ -0,0 +1,7 @@
+//  função anonima
+
+let media = function (n1, n2) {
+  return (n1 + n2) / 2;
+}
+
+console.log(media(10, 8))
